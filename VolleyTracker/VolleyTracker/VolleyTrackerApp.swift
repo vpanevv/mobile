9e7a29cd67d9e81fc32c8b51ba99b524d551ff70
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2026-01-27 16:59:36 +0200

    Аdd the project

---
+++ VolleyTracker/VolleyTracker/VolleyTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  VolleyTrackerApp.swift
+//  VolleyTracker
+//
+//  Created by Vladimir Panev on 27/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VolleyTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
